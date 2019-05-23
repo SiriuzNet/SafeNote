57c46f1cfd11b05bef4d18ece69f2aca629462c9
Krzysztof Derecki <kaderecki@gmail.com>
2019-05-24 01:46:18 +0200

    Reading saved file.

--- app/src/main/java/com/digitalpurr/safenote/ContentsFragment.java
+++ app/src/main/java/com/digitalpurr/safenote/ContentsFragment.java
@@ -9,9 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.stream.Collectors;
 
 public class ContentsFragment extends Fragment {
     public ContentsFragment() {
@@ -27,12 +33,13 @@ public class ContentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_contents, container, false);
+        final EditText editText = view.findViewById(R.id.content_text);
+        editText.setText(readFromFile(getActivity()), TextView.BufferType.NORMAL);
         Button button = view.findViewById(R.id.save_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("SafeNote", "ON SAVE");
-                EditText editText = view.findViewById(R.id.content_text);
                 writeToFile(editText.getText().toString(), getActivity());
             }
         });
@@ -40,15 +47,33 @@ public class ContentsFragment extends Fragment {
         return view;
     }
 
-    private void writeToFile(String data, Context context) {
+    private static void writeToFile(final String data, final Context context) {
         try {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("data.txt", Context.MODE_PRIVATE));
             outputStreamWriter.write(data);
             outputStreamWriter.close();
         }
         catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
+            Log.e("SafeNote", "File write failed: " + e.toString());
         }
+    }
+
+    private static String readFromFile(final Context context) {
+        String ret = "";
+        try {
+            InputStream inputStream = context.openFileInput("data.txt");
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                ret = bufferedReader.lines().collect(Collectors.joining("\n"));
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("SafeNote", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("SafeNote", "Can not read file: " + e.toString());
+        }
+        return ret;
     }
 
     @Override
