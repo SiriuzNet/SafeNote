69a667e4afeca880560d8f9ad5b36f9c4bc16157
Krzysztof Derecki <kaderecki@gmail.com>
2019-06-03 23:24:21 +0200

    Add export button.

--- app/src/main/java/com/digitalpurr/safenote/ContentsFragment.java
+++ app/src/main/java/com/digitalpurr/safenote/ContentsFragment.java
@@ -3,6 +3,8 @@ package com.digitalpurr.safenote;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ContentsFragment extends Fragment {
     private String data;
@@ -37,12 +43,23 @@ public class ContentsFragment extends Fragment {
         final EditText editText = view.findViewById(R.id.content_text);
         editText.setText(data, TextView.BufferType.NORMAL);
         data = null;
-        Button button = view.findViewById(R.id.save_button);
-        button.setOnClickListener((v) -> {
+        Button saveButton = view.findViewById(R.id.save_button);
+        saveButton.setOnClickListener((v) -> {
             Log.d("SafeNote", "ON SAVE");
-            button.setText("WRITING");
-            button.setEnabled(false);
-            onSave(getActivity(), editText.getText().toString(), button);
+            saveButton.setText("WRITING");
+            saveButton.setEnabled(false);
+            onSave(getActivity(), editText.getText().toString(), saveButton);
+        });
+        Button exportButton = view.findViewById(R.id.export_button);
+        exportButton.setOnClickListener((v) -> {
+            Intent intentShareFile = new Intent(Intent.ACTION_SEND);
+            intentShareFile.setType("application/octet-stream");
+            intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://com.digitalpurr.safenote.fileprovider/files/"+Consts.CONTAINER_FILE));
+            intentShareFile.putExtra(Intent.EXTRA_SUBJECT,
+                    "SafeNote data export");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+            intentShareFile.putExtra(Intent.EXTRA_TEXT, "EXPORT TIME: "+sdf.format(new Date()));
+            startActivity(Intent.createChooser(intentShareFile, "Export"));
         });
         return view;
     }
